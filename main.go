267616a523062ab3719bb046ae4cc0605d026bcd
Michel Vocks <michel.vocks@dbschenker.com>
2018-08-21 13:04:21 +0200

    Reduced time

--- main.go
+++ main.go
@@ -11,10 +11,8 @@ func CreateUser(args sdk.Arguments) error {
 	log.Println("CreateUser has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("CreateUser has been finished!")
-
 	return nil
 }
 
@@ -22,10 +20,8 @@ func MigrateDB(args sdk.Arguments) error {
 	log.Println("MigrateDB has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("MigrateDB has been finished!")
-
 	return nil
 }
 
@@ -33,10 +29,8 @@ func CreateNamespace(args sdk.Arguments) error {
 	log.Println("CreateNamespace has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("CreateNamespace has been finished!")
-
 	return nil
 }
 
@@ -44,10 +38,8 @@ func CreateDeployment(args sdk.Arguments) error {
 	log.Println("CreateDeployment has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("CreateDeployment has been finished!")
-
 	return nil
 }
 
@@ -55,10 +47,8 @@ func CreateService(args sdk.Arguments) error {
 	log.Println("CreateService has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("CreateService has been finished!")
-
 	return nil
 }
 
@@ -66,10 +56,8 @@ func CreateIngress(args sdk.Arguments) error {
 	log.Println("CreateIngress has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("CreateIngress has been finished!")
-
 	return nil
 }
 
@@ -77,10 +65,8 @@ func Cleanup(args sdk.Arguments) error {
 	log.Println("Cleanup has been started!")
 
 	// lets sleep to simulate that we do something
-	time.Sleep(15 * time.Second)
-
+	time.Sleep(5 * time.Second)
 	log.Println("Cleanup has been finished!")
-
 	return nil
 }
 
